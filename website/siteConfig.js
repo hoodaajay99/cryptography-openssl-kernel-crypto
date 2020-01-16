a299be7f0b95ea4d09b975ec896fc94bd588fd29
Ajay Hooda <hoodaajay@gmail.com>
2020-01-16 13:18:56 +0530

    updated links

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -38,7 +38,7 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'doc1', label: 'Docs'}
+    {doc: '02-Cryptography-Introduction/02-What-is-Cryptography/What-is-Cryptography', label: 'START THE COURSE'}
   ],
 
   // If you have users set above, you add it here:
